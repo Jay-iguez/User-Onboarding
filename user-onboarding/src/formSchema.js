0a42f910e7c28b0ea6535a7a821ad56d86299d6c
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-07-21 15:59:42 -0700

    Create formSchema basis in formSchemaJS

---
+++ user-onboarding/src/formSchema.js
@@ -0,0 +1,31 @@
+import * as Yup from 'yup'
+
+const formSchema = Yup.object().shape({
+    name: Yup
+    .string()
+    .trim()
+    .required("You must create a username.")
+    .min(5, "Your username must be at least 5 characters"),
+    email: Yup
+    .string()
+    .email("You must have a valid email address.")
+    .required("You must have a valid email addresss."),
+    password: Yup
+    .string()
+    .trim()
+    .required("You must creata a valid password.")
+    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .matches(/\d/, 'Password must contain at least one number')
+    .matches(/[!@#$%^&*()]/, 'Password must contain at least one special character')
+    .matches(/^\S*$/, 'Password must not contain spaces'),
+    terms: Yup
+    .boolean().oneOf([true], "You need to accept our super-duper terms."),
+    money: Yup
+    .string()
+    .required("You must select a wage you receive")
+})
+
+
+
+export default formSchema
