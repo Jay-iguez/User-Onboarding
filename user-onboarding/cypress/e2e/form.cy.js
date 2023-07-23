e9b79eaeca805fe4bb8ce64a5a6a1a7628af2a75
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-07-22 19:00:18 -0700

    Create variables for use in form.cy.js

--- user-onboarding/cypress/e2e/form.cy.js
+++ user-onboarding/cypress/e2e/form.cy.js
@@ -3,8 +3,16 @@ describe("Form Tests", function() {
     beforeEach(() => {
         cy.visit("http://localhost:3001")
     })
-    it("Test run", function() {
-        
+
+    it("Insanity is... not making a dummy test", function() {
         cy.contains("form")
     })
+
+    const nameInput = () => cy.get("input[name=name]")
+    const emailInput = () => cy.get("input[name=email]")
+    const passwordInput = () => cy.get("input[name=password]")
+    const termsInput = () => cy.get("input[name=terms]")
+    const incomeCheck = () => cy.get("input[name=money]")
+
+    
 })
